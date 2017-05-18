9de4ea013f53f0b403437bc4faa87cc672efd2a1
Yuthasak Tanruengsri <yuthasak.tanruengsri@zalando.de>
2017-05-18 15:55:05 +0200

    initial setup library

--- postcss.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    plugins: [
-        /*eslint-disable*/
-        require('precss')({}),
-        require('postcss-cssnext')({ browsers: ['> 1%', 'IE 11'], cascade: false }),
-        /*eslint-enable*/
-    ],
-};
