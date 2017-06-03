cdd3d7cd38e31765e44ecc1af334e2da9771ee57
Yuthasak Tanruengsri <yuthasak.tanruengsri@zalando.de>
2017-06-03 19:17:56 +0200

    remove first tab story

--- src/components/Tabs/Tabs.stories.js
+++ src/components/Tabs/Tabs.stories.js
@@ -8,25 +8,6 @@ const tabContent2 = 'Nam liber tempor cum soluta nobis eleifend option congue ni
 const tabContent3 = 'Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.';
 
 storiesOf('Tabs', module)
-    .add('Specification', () => (
-        <ul>
-            <li>
-                Render tab content on demand
-            </li>
-            <li>
-                Support async content
-            </li>
-            <li>
-                Render tab content once (No re-rendering needed)
-            </li>
-            <li>
-                Scrollable tab headline
-            </li>
-            <li>
-                Add/Remove tabs
-            </li>
-        </ul>
-    ))
     .add('Basic', () => (
         <Tabs
             defaultActiveKey="t1"
