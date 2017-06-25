0d2b61d86b72624ceaee1e9a1f81d5765ea35c04
Yuthasak Tanruengsri <yuthasak.tanruengsri@zalando.de>
2017-06-25 23:06:32 +0200

    upgrade to webpack 2 and storybook 3

---
+++ src/components/Grid/Grid.stories.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+storiesOf('Grid', module)
+    .add('basic', () => (
+        <div>
+            Test
+        </div>
+    ));
